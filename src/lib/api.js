21b2f83e6144e842854b7a8e268c75f8cbaabfdb
shemil01 <shemilmk1033@gmail.com>
2025-08-25 17:00:31 +0530

    updated

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: "https://attendancesystem-server-joov.onrender.com/api",
-  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
