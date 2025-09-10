d87d3bd023fe1552c6d2eb3e4923383ada5466f2
shemil01 <shemilmk1033@gmail.com>
2025-09-10 19:39:48 +0530

    updated

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -29,7 +29,7 @@ export const authOptions = {
           }
 
           if (!res.ok || data.status !== "success") {
-            return null; // failed login
+            return null; 
           }
 
           return {
