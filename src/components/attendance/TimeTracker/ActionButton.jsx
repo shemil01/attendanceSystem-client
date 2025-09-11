3ff8d400cbc8ef5ba6de9383365ad88ab1ff1f4a
shemil01 <shemilmk1033@gmail.com>
2025-09-11 14:48:37 +0530

    updated

--- src/components/attendance/TimeTracker/ActionButton.jsx
+++ src/components/attendance/TimeTracker/ActionButton.jsx
@@ -36,7 +36,7 @@ export default function ActionButtons({
         </button>
       )}
 
-      {!isCheckedOut && (
+      {!isCheckedOut && !isOnBreak && (
         <button
           onClick={onCheckOut}
           disabled={isProcessing}
